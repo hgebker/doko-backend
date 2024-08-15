02f31fd88be335b610e3e3cb53836b432b480b63
Hannes Gebker <hannes.gebker@uni-muenster.de>
2024-08-16 01:48:15 +0200

    DBACK Move files to modules

---
+++ backend/src/main/java/com/hgebk/dokobackend/modelassembler/CashReportModelAssembler.java
@@ -0,0 +1,21 @@
+package com.hgebk.dokobackend.modelassembler;
+
+import com.hgebk.dokobackend.controller.ReportController;
+import com.hgebk.dokobackend.dto.CashReportDTO;
+import org.springframework.hateoas.EntityModel;
+import org.springframework.hateoas.server.RepresentationModelAssembler;
+import org.springframework.stereotype.Component;
+
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.linkTo;
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.methodOn;
+
+@Component
+public class CashReportModelAssembler
+        implements RepresentationModelAssembler<CashReportDTO, EntityModel<CashReportDTO>> {
+
+    @Override
+    public EntityModel<CashReportDTO> toModel(CashReportDTO cashReport) {
+        return EntityModel.of(cashReport)
+                          .add(linkTo(methodOn(ReportController.class).getCashReport()).withSelfRel());
+    }
+}
